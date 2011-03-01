54abaa1859c797fc8b9d6a4fd533be8ab1b4e49e
Dennis Flanigan <dennisf@ucar.edu>
2011-03-01 15:25:58 -0700

    Fixed notify_proc example

--- INotify/notify_proc.rb
+++ INotify/notify_proc.rb
@@ -13,7 +13,7 @@ processThread = Thread.new do
 
   notifier = INotify::Notifier.new
 
-  notifier.watch("/scr/ctm/dennisf/watch_dir", :access, :create, :moved_to, :delete, :moved_from, :recursive, &event_proc) 
+  notifier.watch("/scr/ctm/dennisf/watch_dir1", :access, :create, :moved_to, :delete, :moved_from, :recursive, &event_proc) 
 
   while true do
     # Wait 5 seconds for an event then give up
