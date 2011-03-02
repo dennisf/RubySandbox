66c5d0c0c5c81ea3aff6dcc27d5a70bbe6bcee1b
Dennis Flanigan <dennisf@ucar.edu>
2011-03-02 13:27:15 -0700

    Added test for :is_dir in event array

--- INotify/notify_proc.rb
+++ INotify/notify_proc.rb
@@ -6,14 +6,24 @@ puts "Ruby Version: #{RUBY_VERSION}"
 puts "Notify Example"
 
 event_proc = lambda do |event|
-    puts "name: #{event.absolute_name} event: #{event.flags}"
+
+  flags = event.flags
+
+  if flags.include? :is_dir
+    flags.delete :is_dir
+    puts "directory event: #{flags} path:#{event.absolute_name}"
+  else
+    puts "file event: #{flags}  name: #{event.absolute_name}"
+  end
+
 end
 
 processThread = Thread.new do
 
   notifier = INotify::Notifier.new
 
-  notifier.watch("/scr/ctm/dennisf/watch_dir1", :access, :create, :moved_to, :delete, :moved_from, :recursive, &event_proc) 
+  notifier.watch("/scr/ctm/dennisf/watch_dir1",  :create, :moved_to, :delete, :moved_from, :recursive, &event_proc) 
+  notifier.watch("/scr/ctm/dennisf/watch_dir2",  :create, :moved_to, :delete, :moved_from, :recursive, &event_proc) 
 
   while true do
     # Wait 5 seconds for an event then give up
