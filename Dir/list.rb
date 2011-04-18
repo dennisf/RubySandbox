dcbe949568c95f7cb73d252b973f7a29b1977483
Dennis Flanigan <dennisf@ucar.edu>
2011-04-18 12:49:15 -0600

    Adding Dir and File example

---
+++ Dir/list.rb
@@ -0,0 +1,19 @@
+#! /bin/env ruby
+
+dir_name = "/scr/ctm/dennisf/watch_dir1"
+
+dir = Dir.new dir_name
+
+dir.each do |entry|
+
+
+  abs_path = File.join dir_name,entry
+
+  if File.file? abs_path
+    puts "Entry : #{entry} is a file"
+  else
+    puts "Entry : #{entry} is not a file"
+  end
+
+end
+
