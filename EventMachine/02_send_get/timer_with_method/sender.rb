c683df7b45512c4db22644138d62295bb665fb3d
Dennis Flanigan <dennisf@ucar.edu>
2011-05-12 12:13:45 -0600

    code to break connection and exit EventMachine event loop

--- EventMachine/02_send_get/timer_with_method/sender.rb
+++ EventMachine/02_send_get/timer_with_method/sender.rb
@@ -4,6 +4,9 @@ require 'eventmachine'
 require 'time'
 
 
+def exit_loop
+  EventMachine::stop_event_loop
+end
 
 module EventTalker 
 
@@ -12,16 +15,22 @@ module EventTalker
     message = "Message at #{deltaTime.floor} seconds"
     puts "Sending #{message}"
     send_data message
+
+    if deltaTime > 10
+      EventMachine::cancel_timer @timer_sig
+      close_connection
+    end
   end
 
   def post_init
     puts "Connection established"
     @startTime = Time.now
-    EventMachine::add_periodic_timer(2, method(:talker))
+    @timer_sig = EventMachine::add_periodic_timer 2, method(:talker)
   end
 
   def unbind
-    puts "Connection terminated"
+    puts "Connection terminated, Program  exit in 3 seconds"
+    EventMachine::add_timer 3 , method(:exit_loop)
   end
 
 end
