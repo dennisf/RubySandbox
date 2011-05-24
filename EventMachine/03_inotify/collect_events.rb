1f3874edc803dcd2fee1364fd221e32a982f7a10
Dennis Flanigan <dennisf@ucar.edu>
2011-05-24 15:23:24 -0600

    collect events sends request to send events

--- EventMachine/03_inotify/collect_events.rb
+++ EventMachine/03_inotify/collect_events.rb
@@ -6,12 +6,17 @@ require 'socket'
 
 module EventListener
 
+  include EM::P::ObjectProtocol
 
   def post_init
     @peer_port, @peer_ip = Socket::unpack_sockaddr_in get_peername
     peer_info = Socket::getnameinfo get_peername
     @peer_host = peer_info[0]
     puts "Connection created from #{@peer_host}"
+
+    send_object({:request => "add",:path =>"/scr/ctm/dennisf/watch_dir1"})
+
+    EventMachine::add_timer 15,method(:send_terminate)
   end
 
   def receive_data data
@@ -21,13 +26,17 @@ module EventListener
   def unbind
     puts "Connection terminated from #{@peer_host}"
   end
+  
+  def send_terminate
+    puts "Asking #{@peer_host} to terminate"
+    send_object({:request => "terminate"})
+  end
 
 end
 
 EventMachine::run do
   host,port = "0.0.0.0",8090
   EventMachine::start_server host,port,EventListener
-  puts "Called start_server"
 end
 
 puts "bye"
