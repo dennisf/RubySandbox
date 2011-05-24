ef28a289ca479e8a0c2a0a258758452f377ee641
Dennis Flanigan <dennisf@ucar.edu>
2011-05-24 10:00:31 -0600

    Adding inotify example using method to pass the callback to handle the event processing

---
+++ INotify/notify_method.rb
@@ -0,0 +1,45 @@
+#! /bin/env ruby
+
+require "rb-inotify"
+
+puts "Ruby Version: #{RUBY_VERSION}"
+puts "Notify Example"
+
+def process_event(event)
+
+  flags = event.flags
+
+  if !flags.include? :isdir
+    puts "   #{Time.now} file event: #{flags}  name: #{event.absolute_name}"
+  else
+    puts "   #{Time.now} directory event: #{flags} path:#{event.absolute_name}"
+  end
+
+end
+
+processThread = Thread.new do
+
+  notifier = INotify::Notifier.new
+
+  notifier.watch("/scr/ctm/dennisf/watch_dir1",  :create, :moved_to, :delete, :close_write,  :recursive, &method(:process_event)) 
+
+  while true do
+    # Wait 5 seconds for an event then give up
+    puts "Entering wait at #{Time.now}"
+    if IO.select([notifier.to_io], [], [], 5)
+      notifier.process
+    end
+  end
+
+end
+
+processThread.join
+
+
+
+
+
+
+
+
+
