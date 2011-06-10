78917a73364c86629775f0674cd0001d8259e68c
Dennis Flanigan <dennisf@ucar.edu>
2011-06-10 13:11:50 -0600

    renamed the hello scripts so that the script names are numbered.

---
+++ 01_hello/03_hat.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env ruby
+
+number_of_hats = 4
+hat_size = 6.5
+
+printf "I have %d hats \n",number_of_hats  
+
+printf "My hat size is %3.1f\n",hat_size
+
+
+
--- 01_hello/hat.rb
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env ruby
-
-printf "I have %d hats \n",4  
-
-
