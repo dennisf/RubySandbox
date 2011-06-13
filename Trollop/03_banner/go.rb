17ed6a61032090e585ce7d4121a6c89e989559d5
Dennis Flanigan <dennisf@ucar.edu>
2011-06-13 16:44:43 -0600

    Adding Trollep banner example

---
+++ Trollop/03_banner/go.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/env ruby
+
+require 'pp'
+require 'trollop'
+
+version_string = "version 0.0.2"
+
+begin
+
+  options = Trollop::options do
+    version version_string
+    banner <<-EOS
+
+go.rb (#{version_string}):
+  Test program to see how Trollops banner and version functions.
+EOS
+    opt :number, "Set number",:default => 5
+    opt :color, "Set color",:default => "green"
+  end
+
+  pp "options:"
+  pp options
+
+rescue SystemExit
+  # Don't do anything here.
+rescue Exception => boom
+  puts "Exception class: #{boom.class}"
+  puts "Exception (go.rb): #{boom}"
+end
+
+
+
+
+
