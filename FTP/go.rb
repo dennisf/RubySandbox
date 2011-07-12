7de512c7f70f64771677c065790fd39e70e2eb8d
Dennis Flanigan <dennisf@ucar.edu>
2011-07-12 16:18:01 -0600

    Adding FTP example

---
+++ FTP/go.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/env ruby
+
+require 'net/ftp'
+
+begin
+  ftp_connection = Net::FTP::new('lcatalog.eol.ucar.edu')
+  ftp_connection.login('anonymous','rubytest@ucar.edu')
+
+  ftp_connection.chdir('pub/incoming/catalog/test_area')
+
+  file_list = ftp_connection.list()
+
+  file_list.each do |file_listing|
+    puts file_listing
+  end
+
+  ftp_connection.putbinaryfile('small.file')
+
+  ftp_connection.close()
+  
+rescue Exception => boom
+  puts "Exception class  : #{boom.class}"
+  puts "Exception message: #{boom}"
+end
+
+
+
+
