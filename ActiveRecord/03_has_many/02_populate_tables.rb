66d4c407de48ff5134e209325ba07677f7a8b4b5
Dennis Flanigan <dennisf@ucar.edu>
2011-07-15 17:03:44 -0600

    Finished ActiveRecord population example

--- ActiveRecord/03_has_many/02_populate_tables.rb
+++ ActiveRecord/03_has_many/02_populate_tables.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+#$DEBUG = true
+
 require 'active_record'
 require 'pp'
 require 'logger'
@@ -21,10 +23,34 @@ begin
   ActiveRecord::Base.logger = Logger.new(STDERR)
   ActiveRecord::Base.establish_connection(dbconfig)
 
+  class Product < ActiveRecord::Base
+    has_many :product_files
+  end
 
-  products = ["goes-13-vis","goes-13-ir","gv-flight-track"]
+  class ProductFile < ActiveRecord::Base
+    belongs_to :product
+  end
 
-  start_time = Time::parse("May 22 13:00:00 UTC")
+  start_time    = Time::parse("May 22 13:00:00 UTC")
+  goes_products = ["goes-13-vis","goes-13-ir"]
+
+  goes_times = Array.new
+  20.times do | index |
+    goes_times.push(start_time + (index * 15 * 60))
+  end
+
+  goes_products.each do |product|
+    product_row = Product.create(:name => product, :last_sent => Time.now())
+    goes_times.each do | product_time |
+      time_string = product_time.strftime("%Y%m%d-%H%M%S")
+      goes_path = "#{product}_#{time_string}.jpg"
+      product_row.product_files.create(:filepath => goes_path,:status => "new")
+    end
+  end
+  
+  kml_products  = ["gv-flight-track"]
+
+
 
 
 rescue Exception => boom
