3608629aa865309a90d8403b736adca2d467f550
Dennis Flanigan <dennisf@ucar.edu>
2011-09-22 15:10:14 -0600

    Adding nil test example

---
+++ 09_nil_test/nil_test.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+
+puts "hello"
+
+values = Hash.new
+
+values[:one] = "one tree"
+values[:two] = "two birds"
+
+unless one_tree = values[:one] 
+  puts "No Tree"
+end
+
+unless tree_pears = values[:three]
+  puts "No Pears"
+end
+
+
+
