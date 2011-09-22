f746ec1b925a0475eb909e374ff8f3325b352df4
Dennis Flanigan <dennisf@ucar.edu>
2011-09-22 11:58:05 -0600

    Adding first version of mail gem example

---
+++ Mail/from_eol.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env ruby
+
+require 'mail'
+
+
+Mail.defaults do
+  delivery_method :smtp, 
+  { :address              => "smtp.eol.ucar.edu",
+    :port                 => 25
+    :domain               => 'eol.ucar.edu',
+    :user_name            => nil,
+    :password             => nil,
+    :authentication       => nil,
+    :enable_starttls_auto => true  }
+end
+
+Mail.deliver do
+  to 'dennisf@ucar.edu'
+  from 'mail_test@ucar.edu'
+  subject 'ruby mail gem test'
+  body "test mail at #{Time.now()}"
+end
+
