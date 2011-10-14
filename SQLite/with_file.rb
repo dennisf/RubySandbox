fa01bbb3d9918e65e8516b80ed28a353d9ee5729
Dennis Flanigan <dennisf@ucar.edu>
2011-10-14 16:47:55 -0600

    create table code

--- SQLite/with_file.rb
+++ SQLite/with_file.rb
@@ -6,9 +6,27 @@ require 'bundler/setup'
 require 'active_record'
 require 'sqlite3'
 
-puts "ActiveRecord version : #{ActiveRecord::VERSION::STRING}"
-puts "SQLite3 version : #{SQLite3::VERSION}"
+begin
 
-db_config = {:adapter => "sqlite3", :database => "sightings.db" };
+  puts "ActiveRecord version : #{ActiveRecord::VERSION::STRING}"
+  puts "SQLite3 version : #{SQLite3::VERSION}"
 
-ActiveRecord::Base.establish_connection(db_config)
+  db_config = {:adapter => "sqlite3", :database => "events.db" };
+
+  ActiveRecord::Base.establish_connection(db_config)
+
+  ActiveRecord::Schema.define do
+    create_table :events do | table |
+      table.string   :observation, :null => false
+      table.integer  :grade,       :default => 1
+      table.string   :temperature  :null => false
+      table.timestamps
+    end
+  end
+
+rescue Exception => boom
+  puts "Exception : #{boom.class()}"
+  puts "Error : #{boom}"
+end
+
+
